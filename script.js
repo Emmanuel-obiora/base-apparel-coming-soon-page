99c2c9e6736e5435b75570a1581ce4bd35550d65
Obiora Emmanuel <emmanuelobiora94@gmail.com>
2023-05-12 11:37:49 +0100

    Resolving code bugs

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
 const sub = document.getElementById('clickable');
 
-sub.addEventListener('click', () => {
+sub.addEventListener('submit', (e) => {
+    e.preventDefault();
+
     const email = document.getElementById('userMail').value;
 
     if(email === ""){
@@ -9,14 +11,14 @@ sub.addEventListener('click', () => {
 
         mail.classList.add('input-error');
     }
-    else if(!isValid(email)){
+    if(!isValid(email)){
         const mail = document.getElementById('userMail').parentNode.parentNode;
         const message = document.getElementById('newHint');
 
         mail.classList.add('input-error');
         message.innerText = 'Please provide a valid email'
     }
-    else {
+    else if(isValid(email)) {
         const mail = document.getElementById('userMail').parentNode.parentNode;
 
         mail.classList.remove('input-error');
@@ -25,6 +27,6 @@ sub.addEventListener('click', () => {
 });
 
 function isValid(email) {
-    var re = /\S+@\S+\.\S+/;
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return re.test(email);
 }
